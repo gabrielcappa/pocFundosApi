2522684202ec0df5a1ac256b673bafa54fe8f6cb
Gabriel Cappa <gabrielcappa@id.uff.br>
2021-03-20 20:14:22 -0300

    inicio com boostrap

---
+++ src/main/java/com/cappa/pocFundosApi/PocFundosApiApplication.java
@@ -0,0 +1,13 @@
+package com.cappa.pocFundosApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocFundosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocFundosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cappa/pocFundosApi/PocFundosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cappa.pocFundosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocFundosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
