8f702a684536066519c7d17e2a20a58de76940ef
Gabriel Cappa <gabrielcappa@id.uff.br>
2021-03-21 15:02:03 -0300

    adicionando response entity

--- src/main/java/com/cappa/pocFundosApi/controller/FundoController.java
+++ src/main/java/com/cappa/pocFundosApi/controller/FundoController.java
@@ -4,6 +4,7 @@ import com.cappa.pocFundosApi.model.FundoEntity;
 import com.cappa.pocFundosApi.repository.FundoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,7 +20,8 @@ public class FundoController {
     private FundoRepository repository;
 
     @GetMapping
-    public List<FundoEntity> getFundos(){
-        return repository.findAll();
+    public ResponseEntity<List<FundoEntity>> getFundos(){
+        List<FundoEntity> fundos = repository.findAll();
+        return new ResponseEntity< List<FundoEntity>>(fundos,HttpStatus.OK);
     }
 }
